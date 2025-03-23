d3764d37707888dc179e13ffcf35fb0a7dc12616
Tyler Mueller <muelltyl@gmail.com>
2025-03-23 13:13:03 -0500

    changed styling library from Tailwind to Twind

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+import twind from "$fresh/plugins/twindv1.ts";
+import twindConfig from "./twind.config.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [twind(twindConfig)],
 });
